6f5b61fb3f694cc638ab388e95668fd771ad26fe
KilyongJeong <uzoolove@gmail.com>
2021-11-15 16:18:30 +0900

    .

--- workspace/mulpang/routes/index.js
+++ workspace/mulpang/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/today.html');
+});
+// http://localhost/today.html -> req.params.page = 'today'
+router.get('/:page.html', function(req, res, next) {
+  res.render(req.params.page);
 });
 
 module.exports = router;
